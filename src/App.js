0bbba8c6189d541e1650f6b8a0a347c11e2cf8fc
Alanmg46 <alanmg46@gmail.com>
2018-08-31 17:14:07 -0300

    React App - State

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cor: white,
+      contador: 0
+    };
+  }
 
-import './App.css';
-import Header from './components/Header';
-import NewUser from './components/NewUser';
+  trocarCor = (e) => {
+    let contador = this.state.contador;
+    let cor = (++contador % 2) == 0 ? 'red': 'blue';
+    this.setState({
+      cor: cor,
+      contador: contador
+    });
+  }
 
-class App extends Component {
   render() {
+    const estilo = {
+      padding: 10,
+      background: this.state.cor,
+    }
     return (
-      <div>
-        <Header />
-        <NewUser />
-      </div>
+      <h1 style={estilo} onClick={this.trocarCor}>
+        {this.props.texto}
+      </h1>
     );
   }
 }
-
-export default App;
 
